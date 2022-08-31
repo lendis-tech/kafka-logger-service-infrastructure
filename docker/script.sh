cdff2ca4eef21f31c7cd61820eda81e028db0446
George Wachira <kuria.wachira25@gmail.com>
2022-08-31 18:50:28 +0200

    Corrected pipeline

--- docker/script.sh
+++ docker/script.sh
@@ -3,16 +3,16 @@
 export AWS_ACCESS_KEY_ID=${1}
 export AWS_SECRET_ACCESS_KEY=${2}
 export AWS_SESSION_TOKEN=${3}
-export AWS_REGION=${3} 
-export SERVICE_NAME=${4}
-export STAGE=${5}
+export AWS_REGION=${4} 
+export SERVICE_NAME=${5}
+export STAGE=${6}
 
 upload_file() {
     FILE=${1}
 
     RELATIVE_PATH=(echo ${FILE} | sed -r 's#/app/##g')
 
-    aws s3 mv ${FILE} s3://lendis-eks-source-maps/${SERVICE_NAME}/${STAGE}/${RELATIVE_PATH} --region ${AWS_REGION} \;
+    aws s3 mv ${FILE} s3://lendis-eks-source-maps/${SERVICE_NAME}/${STAGE}/${RELATIVE_PATH} --region ${AWS_REGION}
 }
 
 export -f upload_file
